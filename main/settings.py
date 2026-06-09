48122b07e32e461742ba55fcc42c13ce37d0ffeb
Jaganath-mk <jaganathmk2020.mgce@gmail.com>
2026-06-09 17:41:12 +0530

    Fix CSRF_TRUSTED_ORIGINS with https scheme

--- main/settings.py
+++ main/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = 'django-insecure-9&30n*=1hn17t$wduaq&k7thpysn9*g-z9em_nyyl6vrxx1#mt
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'https://portfolio-production-3546c.up.railway.app',
+    'portfolio-production-3546c.up.railway.app',
     '127.0.0.1',
     'localhost'
 ]
